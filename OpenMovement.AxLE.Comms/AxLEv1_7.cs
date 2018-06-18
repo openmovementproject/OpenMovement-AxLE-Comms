11c7664537c2a281994941882d40befed1901e18
Gerard Wilkinson <gerard.wilkinson@me.com>
2018-06-18 16:03:54 +0100

    Adding 1.7 support.

---
+++ OpenMovement.AxLE.Comms/AxLEv1_7.cs
@@ -0,0 +1,9 @@
+﻿using OpenMovement.AxLE.Comms.Interfaces;
+
+namespace OpenMovement.AxLE.Comms
+{
+    public class AxLEv1_7 : AxLEv1_6
+    {
+        public AxLEv1_7(IAxLEDevice device, string serial) : base(device, serial) { }
+    }
+}
